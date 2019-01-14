21fec2b0393e4ad2bcea27b7a1fe6f1c2a05578b
indietarot <46663566+indietarot@users.noreply.github.com>
2019-01-14 12:08:59 +0700

    Update reply.php

--- Desktop/line-bot-unlimit_friend-master/reply.php
+++ Desktop/line-bot-unlimit_friend-master/reply.php
@@ -1,10 +1,12 @@
 <?php
 
 require "vendor/autoload.php";
-
 $access_token = 'JNHYZM4lvcCnJeEIRIgR76kxbbzh8ZIHL0wW+vfeCQK2Fa6i5fHGeMdet3ZHvFYqMbCqPWnXzHodOhMqpa+4O+QjjSJKP0ZQb/4vPXoUNhzQYpqzaNMckhjnpejnt21Sjtp10JMgxoejuxO9UQTofgdB04t89/1O/w1cDnyilFU=';
-
 $channelSecret = '83cbbcfa65de5592e3da84630e7375e3';
+
+echo "<input type='Text' value ='' name='lineid'>"
+
+
 // ID ที่ต้องการส่งไปหา
 $pushID = 'U2c568bbaf0ae23584c8e6f2b5e36cad3';
 
